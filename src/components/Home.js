f26033900c9a817169542d468d8b4c11faa99155
github username <github email address>
2020-06-10 20:52:24 -0700

    adding social icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Social from './Social'
 
 class Home extends React.Component {
 
     render() {
         return (
             <>
-            <div className="social">
-            
+            <div>
+                <Social />
             </div>
             <div className="home">
                 <p>Hello, I am Juny, Web Developer.</p>
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons'
 
 class Social extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className="social">
+                <ul>
+                <li><SocialIcon url="http://linkedin.com/in/junyoung-mcardle" /></li>
+                <li><SocialIcon url="https://www.facebook.com/junyoung.mcardle" /></li>
+                <li><SocialIcon url="https://github.com/junymc" /></li>
+                </ul>
             </div>
         )
     }
