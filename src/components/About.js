0246a337f4a12be72906665e6d57baa84a6fd433
github username <github email address>
2020-07-28 00:12:11 -0700

    fixing content and adding CSS

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,10 @@ class About extends React.Component {
     render() {
         return (
             <div className="text-zone">
-                <p>Full stack web developer with a background in customer service and transitioned career from sales to software engineering. </p>
-                <p>Consistently recognized for high service levels and teamwork skills impacting project successes and achieving goals.</p>
-                <p>Have combined creative skills from a background in art and graphic design, passionate about creating artistic work with strong visual sense and creativity.</p>
-                <p>Enjoy facing a new challenge and solving the problem.</p>
-                <p>A Quick learner and responsible person so complete the job in time with a good quality of work.</p>
+                <h1>About Me</h1>
+                <p>Full stack web developer with a background in customer service and transitioned career from sales to software engineering. Also have combined creative skills from a background in art and graphic design, passionate about creating artistic work with strong visual sense and creativity.</p>
+                <p>Well-organised person, problem solver. Fan of HGtv tv series, beach, sify and, always love travling. A family person, mother of two fractious boys, therefore remote employment is preferred.</p>
+                <p>A Quick learner, a team player and responsible person so complete the job in time with a good quality of work.</p>
             </div>
         )
     }
