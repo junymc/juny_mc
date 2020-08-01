d74fd77ff7d7f8b03c26c4c1847bd806697e7b89
github username <github email address>
2020-08-01 16:44:58 -0700

    teting dispaying images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,8 +3,16 @@ import React from 'react'
 class Portfolio extends React.Component {
     render() {
         return(
-            <div>
-                
+            <div className="row">
+                <div className="column">
+                    <img src="https://i.imgur.com/6DSgkSh.jpg" alt="Snow" width="400px"/>
+                </div>
+                <div className="column">
+                    <img src="https://i.imgur.com/wpIyens.jpg" alt="Forest" width="400px"/>
+                </div>
+                <div className="column">
+                    <img src="https://i.imgur.com/FgwRQZa.jpg" alt="Mountains" width="400px"/>
+                </div>
             </div>
         )
     }
