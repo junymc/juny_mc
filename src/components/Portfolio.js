e656899e2a7963d204af5c9c53c6a94be68f1b0d
github username <github email address>
2020-08-04 12:59:00 -0700

    installing material-ui/core and material-ui/icons

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,19 +5,19 @@ class Portfolio extends React.Component {
         return(
             <div className="row">
                 <div className="column">
-                    <img src="https://i.imgur.com/dpoJZ5S.jpg" alt="Snow" width="400px"/>
+                    <img src="https://i.imgur.com/dpoJZ5S.jpg" alt="Snow" width="100%"/>
                     <a target="_blank" href="https://github.com/junymc/adventure_challenge"> Adventure Challenge </a>
                 </div>
                 <div className="column">
-                    <img src="https://i.imgur.com/6ZByXqS.png" alt="Forest" width="400px"/>
-                    <a target="_blank" href="https://github.com/junymc/my-love-shoes"> My Love Shoes </a>
-                </div>
-                <div className="column">
-                    <img src="https://i.imgur.com/6DSgkSh.jpg" alt="Mountains" width="400px"/>
+                    <img src="https://i.imgur.com/6DSgkSh.jpg" alt="Forest" width="100%"/>
                     <a target="_blank" href="https://github.com/junymc/rails_project_party_planner"> Party Planner </a>
                 </div>
                 <div className="column">
-                    <img src="https://i.imgur.com/6DSgkSh.jpg" alt="Mountains" width="400px"/>
+                    <img src="https://i.imgur.com/6ZByXqS.png" alt="Mountains" width="100%"/>
+                    <a target="_blank" href="https://github.com/junymc/my-love-shoes"> My Love Shoes </a>
+                </div>
+                <div className="column">
+                    <img src="https://i.imgur.com/6ZByXqS.png" alt="Mountains" width="100%"/>
                     <a target="_blank" href="https://github.com/junymc/sinatra-project-movie-review"> Movie Review </a>
                 </div>
             </div>
